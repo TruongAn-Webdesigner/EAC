bc2bc9886608bb40547881eb287215bbebb5442d
hothilan0101 <73805238+hothilan0101@users.noreply.github.com>
2020-11-20 10:06:14 +0700

    Create place.php

---
+++ admin/view/tour/place.php
@@ -0,0 +1,42 @@
+
+    <h2 class="tour">Place</h2>
+    <div class="boxcenter">
+        <div class="box">
+            <div class="img">
+                <img src="view/img/lisa.jpg" alt="">              
+            </div>
+            <div class="name">
+                <a href="index.php?ctrl=tour&act=tour">G-Dragon - Without You ft. Rosé (Color Coded Lyrics)</a>               
+            </div>
+            
+            
+        </div>
+        <div class="box">
+            <div class="img">
+                <img src="view/img/lisa.jpg" alt="">              
+            </div>
+            <div class="name">
+                <a href="index.php?ctrl=tour&act=tour">G-Dragon - Without You ft. Rosé (Color Coded Lyrics)</a>               
+            </div>
+        
+        </div>
+        <div class="box">
+            <div class="img">
+                <img src="view/img/lisa.jpg" alt="">              
+            </div>
+            <div class="name">
+                <a href="index.php?ctrl=tour&act=tour">G-Dragon - Without You ft. Rosé (Color Coded Lyrics)</a>               
+            </div>
+        
+        </div>
+        <div class="box">
+            <div class="img">
+                <img src="view/img/lisa.jpg" alt="">              
+            </div>
+            <div class="name">
+                <a href="index.php?ctrl=tour&act=tour">G-Dragon - Without You ft. Rosé (Color Coded Lyrics)</a>               
+            </div>
+        
+        </div>
+    </div>
+
