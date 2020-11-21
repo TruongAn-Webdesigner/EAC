ce875ab669390dab08e289a87d68712892f7f9e7
nguyenduccong <71300677+nguyenduccong@users.noreply.github.com>
2020-11-21 19:10:05 +0700

    Update index.php

--- admin/view/home/index.php
+++ admin/view/home/index.php
@@ -82,99 +82,181 @@
                         <!-- end review statis -->
                     </div>
 
+                      <!-- box_statis_rank -->
                     <div class="full">
                             <div class="boxcenter_statis">
-                                <div class="statis_rank">
+                                    <div class="statis_rank">
 
-                                    <div class="box_rank mr2">
-                                        <div class="title_box_rank">
-                                               <div class="text_title_rank">
-                                                    <span>ranks tour </span><br>
-                                                    xếp loại các loại tour
-                                               </div>
+                                       <!-- box rank tour -->
 
-                                               <div class="full">
-                                                   <div class="boxcenter_statis">
-                                                   <div class="content_box_rank">
-                                                        <table class="table table-striped">
-                                                                <thead>
-                                                                    <tr>
-                                                                    <th scope="col">#</th>
-                                                                    <th scope="col">First</th>
-                                                                    <th scope="col">Last</th>
-                                                                    <th scope="col">Handle</th>
-                                                                    </tr>
-                                                                </thead>
-                                                                <tbody>
-                                                                    <tr>
-                                                                    <th scope="row">1</th>
-                                                                    <td>Mark</td>
-                                                                    <td>Otto</td>
-                                                                    <td>@mdo</td>
-                                                                    </tr>
-                                                                    <tr>
-                                                                    <th scope="row">2</th>
-                                                                    <td>Jacob</td>
-                                                                    <td>Thornton</td>
-                                                                    <td>@fat</td>
-                                                                    </tr>
-                                                                    <tr>
-                                                                    <th scope="row">3</th>
-                                                                    <td>Larry</td>
-                                                                    <td>the Bird</td>
-                                                                    <td>@twitter</td>
-                                                                    </tr>
-                                                                </tbody>
-                                                            </table>
+                                        <div class="box_rank mr2">
+                                                <div class="title_box_rank">
+                                                    <div class="text_title_rank">
+                                                            <span>Ranks Tour </span><br>
+                                                            xếp loại các loại tour
                                                     </div>
-                                                   </div>
-                                               </div>
+
+                                                    <div class="full">
+                                                        <div class="boxcenter_statis">
+                                                        <div class="content_box_rank">
+                                                                <table class="table table-striped">
+                                                                        <thead>
+                                                                            <tr>
+                                                                            <th scope="col">#</th>
+                                                                            <th scope="col">First</th>
+                                                                            <th scope="col">Last</th>
+                                                                            <th scope="col">Handle</th>
+                                                                            </tr>
+                                                                        </thead>
+                                                                        <tbody>
+                                                                            <tr>
+                                                                            <th scope="row">1</th>
+                                                                            <td>Mark</td>
+                                                                            <td>Otto</td>
+                                                                            <td>@mdo</td>
+                                                                            </tr>
+                                                                            <tr>
+                                                                            <th scope="row">2</th>
+                                                                            <td>Jacob</td>
+                                                                            <td>Thornton</td>
+                                                                            <td>@fat</td>
+                                                                            </tr>
+                                                                            <tr>
+                                                                            <th scope="row">3</th>
+                                                                            <td>Larry</td>
+                                                                            <td>the Bird</td>
+                                                                            <td>@twitter</td>
+                                                                            </tr>
+
+                                                                        </tbody>
+                                                                    </table>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                       <!-- end box rank tour -->
+
+                                       <!-- box rank user -->
+
+                                        <div class="box_rank">
+                                                <div class="title_box_rank">
+                                                    <div class="text_title_rank">
+                                                        <span>Rank User</span> <br>
+                                                        xếp hàng người dùng đi nhiều tour
+                                                    </div>
+
+                                                    <div class="full">
+                                                        <div class="boxcenter_statis">
+                                                                <div class="content_box_rank">
+                                                                    <table class="table table-striped">
+                                                                            <thead>
+                                                                                <tr>
+                                                                                <th scope="col">#</th>
+                                                                                <th scope="col">First</th>
+                                                                                <th scope="col">Last</th>
+                                                                                <th scope="col">Handle</th>
+                                                                                </tr>
+                                                                            </thead>
+                                                                            <tbody>
+                                                                                <tr>
+                                                                                <th scope="row">1</th>
+                                                                                <td>Mark</td>
+                                                                                <td>Otto</td>
+                                                                                <td>@mdo</td>
+                                                                                </tr>
+                                                                                <tr>
+                                                                                <th scope="row">2</th>
+                                                                                <td>Jacob</td>
+                                                                                <td>Thornton</td>
+                                                                                <td>@fat</td>
+                                                                                </tr>
+                                                                                <tr>
+                                                                                <th scope="row">3</th>
+                                                                                <td>Larry</td>
+                                                                                <td>the Bird</td>
+                                                                                <td>@twitter</td>
+                                                                                </tr>
+                                                                            </tbody>
+                                                                        </table>
+                                                                </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                    </div>
+                                     <!-- end box rank user -->
+
+                                </div>
+                    </div>
+                     <!-- end box_statis_rank -->
+
+                     <!-- box admin -->
+
+                        <div class="full">
+                            <div class="boxcenter_statis">
+                                <div class="box_view_ad">
+
+                                    <div class="title_view_ad">
+                                        <div class="text_view_ad">
+                                            <span>Rank User</span> <br>
+                                             xếp hàng người dùng đi nhiều tour
                                         </div>
                                     </div>
 
-                                    <div class="box_rank">
-                                        <div class="title_box_rank">
-                                            <div class="text_title_rank">
-                                                <span>rank User</span> <br>
-                                                xếp hàng người dùng đi nhiều tour
-                                            </div>
-
-                                            <div class="content_box_rank">
+                                    <div class="full">
+                                        <div class="boxcenter_statis">
+                                            <div class="content_view_ad">
                                                 <table class="table table-striped">
-                                                        <thead>
-                                                            <tr>
+                                                     <thead>
+                                                        <tr>
                                                             <th scope="col">#</th>
-                                                            <th scope="col">First</th>
-                                                            <th scope="col">Last</th>
-                                                            <th scope="col">Handle</th>
-                                                            </tr>
-                                                        </thead>
-                                                        <tbody>
-                                                            <tr>
-                                                            <th scope="row">1</th>
+                                                            <th scope="col">Name</th>
+                                                            <th scope="col">Email</th>
+                                                            <th scope="col">SDT</th>
+                                                            <th scope="col">Position</th>
+                                                            <th scope="col">edit</th>
+                                                            <th scope="col">del</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr>
+                                                            <td scope="row"><img src="../site/img/london.jpg" width=80px height=80px style="border-radius: 50px;"></td>
                                                             <td>Mark</td>
                                                             <td>Otto</td>
                                                             <td>@mdo</td>
-                                                            </tr>
-                                                            <tr>
-                                                            <th scope="row">2</th>
-                                                            <td>Jacob</td>
-                                                            <td>Thornton</td>
+                                                            <td>@mdo</td>
+                                                            <td>@mdo</td>
+                                                            <td>@mdo</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td scope="row"><img src="../site/img/london.jpg" width=80px height=80px style="border-radius: 50px;"></td>
+                                                             <td>Jacob</td>
+                                                             <td>Thornton</td>
+                                                            <td>@mdo</td>
+                                                            <td>@mdo</td>
+                                                            <td>@mdo</td>
                                                             <td>@fat</td>
-                                                            </tr>
-                                                            <tr>
-                                                            <th scope="row">3</th>
+                                                        </tr>
+                                                        <tr>
+                                                            <td scope="row"><img src="../site/img/london.jpg" width=80px height=80px style="border-radius: 50px;"></td>
                                                             <td>Larry</td>
                                                             <td>the Bird</td>
+                                                            <td>@mdo</td>
+                                                            <td>@mdo</td>
+                                                            <td>@mdo</td>
                                                             <td>@twitter</td>
-                                                            </tr>
-                                                        </tbody>
-                                                    </table>
-                                               </div>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
                                         </div>
                                     </div>
 
                                 </div>
                             </div>
-                    </div>
+                        </div>
+
+                     <!-- end box ad -->
                 </div>
