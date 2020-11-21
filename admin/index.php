9f4d494f44a24141d3a076373808853189d78ed4
nguyenduccong <71300677+nguyenduccong@users.noreply.github.com>
2020-11-21 17:48:44 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -25,6 +25,10 @@
     <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 
+    <!-- boottrap -->
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
     
 </head>
 <body>
