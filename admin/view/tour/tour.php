0c694915397c71d184fc96823cf817e1bbdd1ea7
hothilan0101 <73805238+hothilan0101@users.noreply.github.com>
2020-11-23 08:56:09 +0700

    Update tour.php

--- admin/view/tour/tour.php
+++ admin/view/tour/tour.php
@@ -1,91 +1,91 @@
 
-    <h2 class="tour">Tour</h2>
-    <div class="back">
-        <button class="goback" onclick="goBack()">Go Back</button>
+    <h2 class="tourt">Tour</h2>
+    <div class="backt">
+        <button class="gobackt" onclick="goBack()">Go Back</button>
         <script>
             function goBack() {
             window.history.back();
             }
         </script>
     </div>
-    <div class="add">
-        <button class="but"><a href="#">THÊM</a></button>   
+    <div class="addt">
+        <button class="butt"><a href="#">THÊM</a></button>   
     </div>
-    <div class="boxcenter">
-        <div class="box">       
-            <div class="img">
+    <div class="boxcentert">
+        <div class="boxt">       
+            <div class="imgt">
                 <img src="view/img/lisa.jpg" alt="">              
             </div>
-            <div class="name">
+            <div class="tentour">
                 <a href="index.php?ctrl=tour&act=go_tour">G-Dragon - Without You ft. Rosé (Color Coded Lyrics)</a>               
             </div>
-            <div class="xoa">
-                <button class="sua"><a href="#">XÓA</a></button>
-                <button class="sua"><a href="#">SỬA</a></button>
+            <div class="xoat">
+                <button class="suat"><a href="#">XÓA</a></button>
+                <button class="suat"><a href="#">SỬA</a></button>
             </div>
         </div>
-        <div class="box">
-        <div class="img">
+        <div class="boxt">
+        <div class="imgt">
                 <img src="view/img/lisa.jpg" alt="">              
             </div>
-            <div class="name">
+            <div class="tentour">
                 <a href="index.php?ctrl=tour&act=go_tour">G-Dragon - Without You ft. Rosé (Color Coded Lyrics)</a>               
             </div>
-            <div class="xoa">
-                <button class="sua"><a href="#">XÓA</a></button>
-                <button class="sua"><a href="#">SỬA</a></button>
+            <div class="xoat">
+                <button class="suat"><a href="#">XÓA</a></button>
+                <button class="suat"><a href="#">SỬA</a></button>
             </div>
         
         </div>
-        <div class="box">
-        <div class="img">
+        <div class="boxt">
+        <div class="imgt">
                 <img src="view/img/lisa.jpg" alt="">              
             </div>
-            <div class="name">
+            <div class="tentour">
                 <a href="index.php?ctrl=tour&act=go_tour">G-Dragon - Without You ft. Rosé (Color Coded Lyrics)</a>               
             </div>
-            <div class="xoa">
-                <button class="sua"><a href="#">XÓA</a></button>
-                <button class="sua"><a href="#">SỬA</a></button>
+            <div class="xoat">
+                <button class="suat"><a href="#">XÓA</a></button>
+                <button class="suat"><a href="#">SỬA</a></button>
             </div>
         
         </div>
-        <div class="box">
-        <div class="img">
+        <div class="boxt">
+        <div class="imgt">
                 <img src="view/img/lisa.jpg" alt="">              
             </div>
-            <div class="name">
+            <div class="tentour">
                 <a href="index.php?ctrl=tour&act=go_tour">G-Dragon - Without You ft. Rosé (Color Coded Lyrics)</a>               
             </div>
-            <div class="xoa">
-                <button class="sua"><a href="#">XÓA</a></button>
-                <button class="sua"><a href="#">SỬA</a></button>
+            <div class="xoat">
+                <button class="suat"><a href="#">XÓA</a></button>
+                <button class="suat"><a href="#">SỬA</a></button>
             </div>
         
         </div>
-        <div class="box">
-            <div class="img">
+        <div class="boxt">
+            <div class="imgt">
                 <img src="view/img/lisa.jpg" alt="">              
             </div>
-            <div class="name">
+            <div class="tentour">
                 <a href="index.php?ctrl=tour&act=go_tour">G-Dragon - Without You ft. Rosé (Color Coded Lyrics)</a>               
             </div>
-            <div class="xoa">
-                <button class="sua"><a href="#">XÓA</a></button>
-                <button class="sua"><a href="#">SỬA</a></button>
+            <div class="xoat">
+                <button class="suat"><a href="#">XÓA</a></button>
+                <button class="suat"><a href="#">SỬA</a></button>
             </div>
         
         </div>
-        <div class="box">
-        <div class="img">
+        <div class="boxt">
+        <div class="imgt">
                 <img src="view/img/lisa.jpg" alt="">              
             </div>
-            <div class="name">
+            <div class="tentour">
                 <a href="index.php?ctrl=tour&act=go_tour">G-Dragon - Without You ft. Rosé (Color Coded Lyrics)</a>               
             </div>
-            <div class="xoa">
-                <button class="sua"><a href="#">XÓA</a></button>
-                <button class="sua"><a href="#">SỬA</a></button>
+            <div class="xoat">
+                <button class="suat"><a href="#">XÓA</a></button>
+                <button class="suat"><a href="#">SỬA</a></button>
             </div>
         
         </div>
