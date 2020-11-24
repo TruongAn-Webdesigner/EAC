9593985da47756a8ab8918c69c8d1cef889b9f78
TruongAn-Webdesigner <71300924+TruongAn-Webdesigner@users.noreply.github.com>
2020-11-24 15:38:23 +0700

    Update wewillgo.php

--- site/controller/wewillgo.php
+++ site/controller/wewillgo.php
@@ -1,5 +1,28 @@
 <?php
+include_once 'model/database.php';
+include_once 'model/wherewillwego.php';
+include_once 'model/content.php';
+$action = 'index';
+if(isset($_GET['act'])){
+    $action=$_GET['act'];
+}
 
+switch($action){
+    case 'index':
+    $topFeauture=getAllTopFeauture();
+    $allPlace=getAllPlace();
     include_once 'view/wherewillwego/index.php';
+    break;
+
+    case 'content':
+    $id=$_GET['idcontent'];
+    $topFeautureById=getAllTopFeautureById($id);
+    include 'view/content/content.php';
+    break;
+
+    case 'contentbestplace':
+    include_once 'view/content/content.php';
+    break;
+}
 
 ?>
