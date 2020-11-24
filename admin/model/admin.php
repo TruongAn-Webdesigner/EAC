730b375ec9dc45fc64480fd0693644b643610198
nguyenduccong <71300677+nguyenduccong@users.noreply.github.com>
2020-11-24 21:57:09 +0700

    Create admin.php

---
+++ admin/model/admin.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once('database.php');
+
+    function deleteAdmin($id){
+        $sql="delete from admin where id_ad='$id'";
+        return execute($sql);
+    }
+
+
+    function getAdmin(){
+        $sql="SELECT * FROM `admin`";
+        return query($sql);
+      }
+?>
