06de6c069d87f17ba85db7bc4aa8443bf7da89ed
TruongAn-Webdesigner <71300924+TruongAn-Webdesigner@users.noreply.github.com>
2020-11-24 15:38:19 +0700

    Update home.php

--- site/controller/home.php
+++ site/controller/home.php
@@ -1,5 +1,20 @@
 <?php
+    include_once 'model/database.php';
+    include_once 'model/content.php';
+    include_once 'model/wherewillwego.php';
+    $action = 'index';
+    if(isset($_GET['act'])){
+        $action=$_GET['act'];
+    }
 
-    include_once 'view/home/index.php';
+    switch($action){
+        case 'index':
+        $allReview=getAllTopFeauture();
+        include 'view/home/index.php';
+        break;
+    }
 
+
+    
+    
 ?>
