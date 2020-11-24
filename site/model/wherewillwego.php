a6d3fee12c28e22503294a35dbb9aaeefcbf3078
TruongAn-Webdesigner <71300924+TruongAn-Webdesigner@users.noreply.github.com>
2020-11-24 15:39:26 +0700

    Create wherewillwego.php

---
+++ site/model/wherewillwego.php
@@ -0,0 +1,15 @@
+<?php
+    include_once('database.php');
+
+    function getAllTopFeauture(){
+        $sql="select * from diadiem order by id_dd limit 5";
+        return query($sql);
+    }
+
+    function getAllPlace(){
+        $sql="SELECT name_dd,rating_dd,diadiem.id_kv,diadiem.id_dd,name_kv,ngaydang,img_dd,rating from ((diadiem INNER JOIN khuvuc ON diadiem.id_kv=khuvuc.id_kv) INNER JOIN noidungbocuc on diadiem.id_dd=noidungbocuc.id_dd)";
+        return query($sql);
+    }
+
+    
+?>
