67dd08a11d24ce9e9d46f236abdc385fb604eb50
nguyenduccong <71300677+nguyenduccong@users.noreply.github.com>
2020-11-24 21:56:15 +0700

    Update place.php

--- admin/controller/place.php
+++ admin/controller/place.php
@@ -1,5 +1,6 @@
 <?php
-    
+    include_once 'model/place.php';
+
     $action='index';
     if(isset($_GET['act'])){
         $action=$_GET['act'];
@@ -7,11 +8,27 @@
 
     switch($action){
         case 'index':
+            $kv=getAllKV();
             include_once 'view/place/place.php';
         break;
 
-        case 'ct':
-            include_once 'view/place/chitietplace.php';
+        case 'add':
+            include_once 'view/place/addnew.php';
         break;
+
+        case 'insert':
+            $name_kv=$_POST['name'];
+            $img = $_FILES['file_name']['name'];
+
+            $path = '../site/img/'.$img;
+
+            if(move_uploaded_file($_FILES['file_name']['tmp_name'],$path)){
+                addKV($name_kv,$img);//them vao databse
+               }
+
+               /* chuyển trang bằng js */
+               echo'<script>window.location="index.php?ctrl=place&act=index";</script>';
+        break;
+
     }
 ?>
