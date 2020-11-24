35859cee35659fc0dc5f22c23f0899befb3f6088
TruongAn-Webdesigner <71300924+TruongAn-Webdesigner@users.noreply.github.com>
2020-11-24 15:39:21 +0700

    Create content.php

---
+++ site/model/content.php
@@ -0,0 +1,8 @@
+<?php
+    include_once('database.php');
+
+    function getAllTopFeautureById($id){
+        $sql="SELECT * FROM noidungbocuc INNER JOIN diadiem on diadiem.id_dd=noidungbocuc.id_noidung WHERE diadiem.id_dd='$id'";
+        return query($sql);
+    }
+?>
