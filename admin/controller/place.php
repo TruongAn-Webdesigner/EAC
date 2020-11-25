b601ebc090617721115a9594c1b91aa704e108d3
nguyenduccong <71300677+nguyenduccong@users.noreply.github.com>
2020-11-25 07:55:35 +0700

    Update place.php

--- admin/controller/place.php
+++ admin/controller/place.php
@@ -30,5 +30,57 @@
                echo'<script>window.location="index.php?ctrl=place&act=index";</script>';
         break;
 
+        case 'edit':
+            $id = $_GET['id'];
+            $kv = getKVID($id);
+            include 'view/place/editPlace.php';
+        break;
+
+        case 'update':
+            $id= $_POST['id'];
+            $name =$_POST['name'];
+            $img= $_FILES['file_name']['name'];
+            $path = '../site/img/'.$img;
+          if($img!=""){
+              move_uploaded_file($_FILES['file_name']['tmp_name'],$path);
+              updateKVhinh($id,$name,$img);//them vao databse
+          }else{
+            updateKV($id,$name);
+          }
+          echo'<script>window.location="index.php?ctrl=place&act=index";</script>';
+          break;
+
+        case 'del':
+            $id = $_GET['id'];
+            $delete=deleteKV($id);
+            if ($delete) {
+            echo'
+            <script type="text/javascript">
+                swal({
+                title: "Success!",
+                text: "Ban đã xoa thành công.",
+                type: "success",
+                timer: 2000,
+                showConfirmButton: true
+                }, function(){
+                window.location.href ="index.php?ctrl=place";
+                });
+                </script>';
+            }else{
+            echo'
+            <script type="text/javascript">
+                swal({
+                title: "Error!",
+                text: "Không xóa được vì tồn tại khách hàng.",
+                type: "error",
+                timer: 5000,
+                showConfirmButton: true
+                }, function(){
+                window.location.href = "index.php?ctrl=place";
+                });
+                </script>';
+            }
+            $kv=getAllKV();
+            include_once 'view/place/place.php';
     }
 ?>
