362ea8961619bad6d3181fb736de4ac4f2bcbb23
nguyenduccong <71300677+nguyenduccong@users.noreply.github.com>
2020-12-08 08:53:13 +0700

    Create login.php

---
+++ site/model/login.php
@@ -0,0 +1,9 @@
+<?php
+            include_once('database.php');
+
+            function getkh($kh_login){
+                $sql="SELECT * FROM khachhang WHERE email_kh = '$kh_login'";
+                return query($sql);
+            }
+    
+?>
