98af5fc5f35205fda8de3ab68fb09167657d8404
TruongAn-Webdesigner <71300924+TruongAn-Webdesigner@users.noreply.github.com>
2020-12-08 08:45:57 +0700

    Create booktour.php

---
+++ site/model/booktour.php
@@ -0,0 +1,11 @@
+<?php
+    function addToOrder($fname,$address,$email,$phone,$diadiem,$ngaydat)
+    {
+    $query="insert into booktour(hoten,email,sdt,diachi,id_dd,ngaydat)
+    values('$fname','$email','$phone','$address','$diadiem','$ngaydat')";
+    $lastId = executeReturnID($query);
+    return $lastId;//tra ve ma don hang moi vua them
+    }
+    
+?>
+
