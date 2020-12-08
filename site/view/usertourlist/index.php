6cffd20b5fc2cd088075997de798891ab3119e00
TruongAn-Webdesigner <71300924+TruongAn-Webdesigner@users.noreply.github.com>
2020-12-08 18:16:10 +0700

    Update index.php

--- site/view/usertourlist/index.php
+++ site/view/usertourlist/index.php
@@ -4,14 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/usertour.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
 <header>
         <div class="roww">
+        <div class="bg_header_content">
             <div class="boxcenter_content_tip">
-                <div class="bg_header_content">
+                
                     <div class="bg_menu_content">
-                        <nav class="nav">
+                        <nav class="navuser">
                             <div class="bg_logo_content">
                                 <div class="text_logo">
                                     <a href="index.php?ctrl=home&act=index">EAC</a>
@@ -23,7 +26,7 @@
                 
                             
                         </nav>
-                        <div class="left_login">
+                        <div class="left_loginuser">
                             <div class="float_text">
                             
                             <?php
@@ -65,5 +68,35 @@
             </div>
         </div>
     </header>
+
+
+    <article>
+        <div class="roww">
+            <div class="full_bg">
+                <div class="boxcenter_content_tip">
+                <form action="#" method="post">
+                    <div class="card mt-3">
+                        <div class="card-header">
+                        
+                        </div>
+                            <div class="card-body">
+                                <table class="table table-striped table">
+                                
+                                    <tr>
+                                        <td>Mã tour</td>
+                                        <td>Tên khách hàng</td>
+                                        <td>Địa điểm</td>
+                                        <td>Ngày đi</td>
+                                    </tr>
+                            
+                                <?php?>
+
+                                </table>
+                </form>
+
+                </div>
+            </div>
+        </div>
+    </article>
 </body>
 </html>
