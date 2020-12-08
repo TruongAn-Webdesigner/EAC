a3939ff93a5f8cca25eff4a7bb9b31c4db439dfe
TruongAn-Webdesigner <71300924+TruongAn-Webdesigner@users.noreply.github.com>
2020-12-08 08:45:33 +0700

    Update content-tip.php

--- site/controller/content-tip.php
+++ site/controller/content-tip.php
@@ -2,6 +2,7 @@
 include_once 'model/database.php';
 include_once 'model/wherewillwego.php';
 include_once 'model/content.php';
+include_once 'model/content_tip.php';
 $action = 'index';
 if(isset($_GET['act'])){
     $action=$_GET['act'];
@@ -9,19 +10,19 @@ if(isset($_GET['act'])){
 
 switch($action){
     case 'index':
-    $topFeauture=getAllTopFeauture();
-    $allPlace=getAllPlace();
-    include_once 'view/wherewillwego/index.php';
+    $allFeautureTip=getFeautureTip();
+    $allTip=getAllTip();
+    include_once 'view/tip/index.php';
     break;
 
     case 'contenttip':
-    $id=$_GET['idcontenttip'];
-    $topFeautureById=getAllTopFeautureById($id);
+    $id=$_GET['idtip'];
+    $tip=getAllTipById($id);;
     include 'view/content/content-tip.php';
     break;
 
-    case 'contentbestplace':
-    include_once 'view/content/content.php';
+    case 'contentread':
+    include_once 'view/content/content-tip.php';
     break;
 }
 
