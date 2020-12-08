5ed9818d1baafe66bd5274f7b89c4be72aadd433
TruongAn-Webdesigner <71300924+TruongAn-Webdesigner@users.noreply.github.com>
2020-12-08 16:09:31 +0700

    Update content.php

--- site/view/content/content.php
+++ site/view/content/content.php
@@ -46,8 +46,8 @@
             <div class="boxcenter_content">
 
                 <div class="bg_article">
-                <div class="box_position_content1"></div>
-                <!-- /* <div class="box_position_content2"></div> */ -->
+                    <div class="box_position_content1"></div>
+                        <!-- /* <div class="box_position_content2"></div> */ -->
                         <?php
                             foreach($topFeautureById as $tr){
                                 echo'
@@ -71,9 +71,8 @@
                                             </div>
                                     </div>
                                 </div>
-                            
-                        
                             </div>
+
                         <div class="box_img_article">
                             <div class="single-item">
                                 
@@ -94,7 +93,7 @@
                      
                             <h3>About place</h3>
                       
-                        <h5>Where is it?</h5>
+                            <h5>Where is it?</h5>
                             <div class="box_text_content2">
                                 
                                     '.$tr['noidung2'].'
@@ -193,89 +192,157 @@
                         </div>
                                
 
-                </div>
+
+
+
+
+';
+
+                                                if(!isset($_SESSION['user']))//kiem tra xem co dang nhap chua
+                                                    {
+                                                        /* neu chua co */
+                                                        echo'
+                                                    <div class="roww">
+                                                        <div class="bg_link_form">
+                                                            <div class="bg_text">
+                                                                Join with us!
+                                                            </div>
+                                                    
+                                                    
+                                                            <div class="bg-buy-add">
+                                                                            <div class="box_hover_a">
+                                                                            <a id="myBtn">
+                                                                                <span></span>
+                                                                                <span></span>
+                                                                                <span></span>
+                                                                                <span></span>
+                                                                                sign tour
+                                                                            </a>
+                                                                            </div>
+                                                                            <div class="bg_book_tour">
+                                                                            <div id="myModal" class="modal">
+                                                                                <div class="modal-content">
+                                                                                    <div class="close">
+                                                                                        <span>&times;</span>
+                                                                                    </div>
+                                                                                    <div class="boxPay">
+                                                                                    <form action="index.php?ctrl=wewillgo&act=booktour&idcontent='.$tr['id_dd'].'" method="post" enctype="multipart/form-data">   
+                                                                                            <div class="container">
+                                                                                                <h1>Book tour: '.$tr['name_dd'].'</h1>
+                                                                                                    <hr>
+                                                                                                        <label for="diadiem">Mã địa điểm</label>
+                                                                                                        <div class="input_bg">
+                                                                                                        <input type="text" placeholder="Enter Number" name="diadiem" value="'.$tr['id_dd'].' "readonly >
+                                                                                                        </div>
+                                                                                                        <div class="input_bg">
+                                                                                                        <label for="ngaydi">Ngày đi dự kiến</label>
+                                                                                                        <input type="text" placeholder="Enter Number" name="ngaydi" value="'.$tr['ngaydang'].' "readonly >
+                                                                                                        </div>
+                                                                                                        <label for="fullname">Họ tên</label>
+                                                                                                        <input type="text" placeholder="Nhập tên của bạn" name="fullName" required>
+                                                    
+                                                                                                        <label for="email">Email</label>
+                                                                                                        <input type="text" placeholder="Nhập Email" name="email" required>
+                                                                                                            
+                                                                                                        <label for="phone">Số điện thoại</label>
+                                                                                                        <input type="tel" placeholder="nhập số điện thoại" name="phone" required>
+                                                    
+                                                                                                    
+                                                                                                <div class="clearfix">
+                                                                                                    <button type="submit" class="button">Done!</button>
+                                                                                                    <button type="button" id="cancel" class="button">Lets me think</button>
+                                                                                                </div>
+                                                                                            </div>
+                                                                                       
+                                                                                    
+                                                                              
+                                                    
+                                                    
+                                                       
+                                                                         </form>
+                                                                        </div>
+                                                                          </div>
+                                                    
+                                                    </div>';
+                                                    } 
+                                                    else{
+                                                       
+                                                        echo'
+                                                        <div class="roww">
+                                                            <div class="bg_link_form">
+                                                                <div class="bg_text">
+                                                                    Join with us!
+                                                                </div>
+                                                        
+                                                        
+                                                                <div class="bg-buy-add">
+                                                                                <div class="box_hover_a">
+                                                                                <a id="myBtn">
+                                                                                    <span></span>
+                                                                                    <span></span>
+                                                                                    <span></span>
+                                                                                    <span></span>
+                                                                                    sign tour
+                                                                                </a>
+                                                                                </div>
+                                                                                <div class="bg_book_tour">
+                                                                                <div id="myModal" class="modal">
+                                                                                    <div class="modal-content">
+                                                                                        <div class="close">
+                                                                                            <span>&times;</span>
+                                                                                        </div>
+                                                                                        <div class="boxPay">
+                                                                                        <form action="index.php?ctrl=wewillgo&act=booktour&idcontent='.$tr['id_dd'].'" method="post" enctype="multipart/form-data">   
+                                                                                                <div class="container">
+                                                                                                    <h1>Book tour: '.$tr['name_dd'].'</h1>
+                                                                                                        <hr>
+                                                                                                            <label for="diadiem">Mã địa điểm</label>
+                                                                                                            <div class="input_bg">
+                                                                                                            <input type="text" placeholder="Enter Number" name="diadiem" value="'.$tr['id_dd'].' "readonly >
+                                                                                                            </div>
+                                                                                                            <div class="input_bg">
+                                                                                                            <label for="ngaydi">Ngày đi dự kiến</label>
+                                                                                                            <input type="text" placeholder="Enter Number" name="ngaydi" value="'.$tr['ngaydang'].' "readonly >
+                                                                                                            </div>
+                                                                                                            <label for="fullname">Họ tên</label>
+                                                                                                            <input type="text" placeholder="Nhập tên của bạn" name="fullName" required>
+                                                        
+                                                                                                            <label for="email">Email</label>
+                                                                                                            <input type="text" placeholder="Nhập Email" name="email" required>
+                                                                                                                
+                                                                                                            <label for="phone">Số điện thoại</label>
+                                                                                                            <input type="tel" placeholder="nhập số điện thoại" name="phone" required>
+                                                        
+                                                                                                        
+                                                                                                    <div class="clearfix">
+                                                                                                        <button type="submit" class="button">Done!</button>
+                                                                                                        <button type="button" id="cancel" class="button">Lets me think</button>
+                                                                                                    </div>
+                                                                                                </div>
+                                                                                           
+                                                                                        
+                                                                                  
+                                                        
+                                                        
+                                                           
+                                                                             </form>
+                                                                            </div>
+                                                                              </div>
+                                                        
+                                                        </div>
+                                                        ';
+                                                        
+                                                    }
+                                                    
+                            }
+?>
+
+</div>
             </div>
         </div>
     </article>
 
-<div class="roww">
-    <div class="bg_link_form">
-        <div class="bg_text">
-            Join with us!
-        </div>
 
-
-        <div class="bg-buy-add">
-                        <div class="box_hover_a">
-                        <a id="myBtn">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                            sign tour
-                        </a>
-                        </div>
-                        <div class="bg_book_tour">
-                        <div id="myModal" class="modal">
-                            <div class="modal-content">
-                                <div class="close">
-                                    <span>&times;</span>
-                                </div>
-                                <div class="boxPay">
-                                <form action="index.php?ctrl=wewillgo&act=booktour&idcontent='.$tr['id_dd'].'" method="post" enctype="multipart/form-data">
-
-    
-                       
-                                
-
-                              
-                                  
-                                        <div class="container">
-                                            <h1>Book tour: '.$tr['name_dd'].'</h1>
-                                                <hr>
-                                                    <label for="diadiem">Mã địa điểm</label>
-                                                    <div class="input_bg">
-                                                    <input type="text" placeholder="Enter Number" name="diadiem" value="'.$tr['id_dd'].' "readonly >
-                                                    </div>
-                                                    <div class="input_bg">
-                                                    <label for="ngaydi">Ngày đi dự kiến</label>
-                                                    <input type="text" placeholder="Enter Number" name="ngaydi" value="'.$tr['ngaydang'].' "readonly >
-                                                    </div>
-                                                    <label for="fullname">Họ tên</label>
-                                                    <input type="text" placeholder="Nhập tên của bạn" name="fullName" required>
-
-                                                    <label for="email">Email</label>
-                                                    <input type="text" placeholder="Nhập Email" name="email" required>
-                                                        
-                                                    <label for="phone">Số điện thoại</label>
-                                                    <input type="tel" placeholder="nhập số điện thoại" name="phone" required>
-
-                                                
-                                            <div class="clearfix">
-                                                <button type="submit" class="button">Done!</button>
-                                                <button type="button" id="cancel" class="button">Lets me think</button>
-                                            </div>
-                                        </div>
-                                   
-                                
-                          
-
-
-   
-                     </form>
-                    </div>
-                      </div>
-
-</div>
-                    </div>
-
-
-    </div>
-</div>
-
-';
-}
-?>
     <div class="roww">
         <div class="boxcenter_content">
             <div class="box_choose">
