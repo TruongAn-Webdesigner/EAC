d206495dd5c86c669a1af0416d95798ab233c4ff
TruongAn-Webdesigner <71300924+TruongAn-Webdesigner@users.noreply.github.com>
2020-12-08 08:45:43 +0700

    Update wewillgo.php

--- site/controller/wewillgo.php
+++ site/controller/wewillgo.php
@@ -2,6 +2,7 @@
 include_once 'model/database.php';
 include_once 'model/wherewillwego.php';
 include_once 'model/content.php';
+include_once 'model/booktour.php';
 $action = 'index';
 if(isset($_GET['act'])){
     $action=$_GET['act'];
@@ -18,6 +19,20 @@ switch($action){
     $id=$_GET['idcontent'];
     $topFeautureById=getAllTopFeautureById($id);
     include 'view/content/content.php';
+    break;
+
+    case 'booktour':
+        $id=$_GET['idcontent'];
+    $topFeautureById=getAllTopFeautureById($id);
+        //luu thong tin don hang
+        $fname=$_POST['fullName'];
+        $address=$_POST['address'];
+        $email=$_POST['email'];
+        $phone=$_POST['phone'];
+        $diadiem=$_POST['diadiem'];
+        $ngaydat=date('Y-m-d H:i:s');//ngay thang nam, gio,.. hien tai
+        $madh=addToOrder($fname,$address,$email,$phone,$diadiem,$ngaydat);
+        include 'view/content/content.php';
     break;
 
     case 'contentbestplace':
