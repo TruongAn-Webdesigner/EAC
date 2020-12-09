4820b0670955cbd9bb56e2cc647d9a14d9991f26
TruongAn-Webdesigner <71300924+TruongAn-Webdesigner@users.noreply.github.com>
2020-12-09 12:03:22 +0700

    Update wewillgo.php

--- site/controller/wewillgo.php
+++ site/controller/wewillgo.php
@@ -21,10 +21,10 @@ switch($action){
     case 'content':
     $id=$_GET['idcontent'];
     $topFeautureById=getAllTopFeautureById($id);
-    $email_kh=$_SESSION['user'];/* lấy email của khách hàng */
-    $khachhang=getkh_id($email_kh); /* từ email để truy cập vào database lấy thông ti của khách hàng */
-    $id_kh=$khachhang['id_kh'];/* lấy id khách hàng */
-    $kh_dg=getKH_DG($id_kh,$id);
+    /* $email_kh=$_SESSION['user']; *//* lấy email của khách hàng */
+    /* $khachhang=getkh_id($email_kh); */ /* từ email để truy cập vào database lấy thông ti của khách hàng */
+    /* $id_kh=$khachhang['id_kh']; *//* lấy id khách hàng */
+    /* $kh_dg=getKH_DG($id_kh,$id); */
     include 'view/content/content.php';
     break;
 
