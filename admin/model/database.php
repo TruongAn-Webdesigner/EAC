5d2f2306b85a29070d4e4e483c51c2bcff3bbd53
nguyenduccong <71300677+nguyenduccong@users.noreply.github.com>
2020-12-09 12:17:06 +0700

    Update database.php

--- admin/model/database.php
+++ admin/model/database.php
@@ -3,7 +3,7 @@
    function getConnection(){
         //khai bao server
         $host='localhost';
-        $dbname='lala';
+        $dbname='eac';
         $userName='root';
         $passWord='';
 /*         $options= array(PDO::MYSQL_ATTR_INIT_COMMAND=> "SET NAMES uf8",
