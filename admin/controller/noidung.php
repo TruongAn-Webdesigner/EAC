ff71bf3f5d484691ea162516d076d5dcb86dc2bb
nguyenduccong <71300677+nguyenduccong@users.noreply.github.com>
2020-12-09 12:16:53 +0700

    Update noidung.php

--- admin/controller/noidung.php
+++ admin/controller/noidung.php
@@ -2,7 +2,9 @@
      include_once 'model/home.php';
      include_once 'model/admin.php';
      include_once 'model/login.php';
-    
+     include_once 'model/place.php';
+     include_once 'model/tour.php';
+ 
 
     $action = 'index';
     if(isset($_GET['act'])){
@@ -12,9 +14,19 @@
     switch($action){
         case 'index':
         $id=$_GET['id'];
-        $topFeautureById=getAllTopFeautureById($id);
-        include 'view/noidung/index.php';
+        $dd = getDDID($id);
+        if($dd['trangthai'] == 0 ){
+            echo'<script>alert("Bạn Chưa Thêm Nội Dung Cho Địa Điểm Vui Lòng Thêm Nội Dung")</script>';
+            include 'view/noidung/add.php';
+        }else{
+            $topFeautureById=getAllTopFeautureById($id);
+            include 'view/noidung/index.php';
+        } 
+        
         break;
+
+        case 'add':
+        include 'view/noidung/add.php';
     }
 
     
