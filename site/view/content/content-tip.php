8bb1a7e481cffa3a14240809174f84ec43f46916
TruongAn-Webdesigner <71300924+TruongAn-Webdesigner@users.noreply.github.com>
2020-12-09 12:03:34 +0700

    Update content-tip.php

--- site/view/content/content-tip.php
+++ site/view/content/content-tip.php
@@ -90,7 +90,7 @@
                             foreach($tip as $tc){
                                 echo'
                         <div class="name_tip">
-                            <h3>Plan(title1)</h3>
+                            <h3>Địa Điểm</h3>
                             <div class="text_tip">
                                 <div class="text_tip_content">
                                     <div class="text_title_tip">
@@ -129,7 +129,9 @@
                         </div>
                     </div>
                     <div class="bg_tip_position"></div>
-                    <div class="box_img_tip_right"></div>
+                    <div class="box_img_tip_right">
+                    <img src="img/light.png" alt="">
+                    </div>
                     </div>
                 </div>
                 
@@ -143,7 +145,7 @@
 
 
                         <div class="name_tip">
-                            <h3>belongings (title2)</h3>
+                            <h3>Trải nghiệm</h3>
                             
                             <div class="text_tip3">
                                 <div class="text_tip_content">
@@ -177,7 +179,9 @@
                         </div>
                     </div>
                     <div class="bg_tip_position2"> </div>
-                    <div class="box_img_tip_right"></div>
+                    <div class="box_img_tip_right">
+                                <img src="img/bag.png" alt="">
+                    </div>
                     </div>
                 </div>
                 ';
@@ -189,7 +193,7 @@
 
     </article>
 
-<div class="roww">
+<!-- <div class="roww">
     <div class="bg_link_form">
         <div class="bg_text">
             Join with us!
@@ -247,7 +251,7 @@
 
 
     </div>
-</div>
+</div> -->
     <div class="roww">
         <div class="boxcenter_content_tip">
             <div class="box_choose">
