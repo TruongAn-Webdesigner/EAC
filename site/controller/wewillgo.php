ebeb11097194a6633f1115e9bcf3b7d558861646
TruongAn-Webdesigner <71300924+TruongAn-Webdesigner@users.noreply.github.com>
2020-12-14 12:57:16 +0700

    Update wewillgo.php

--- site/controller/wewillgo.php
+++ site/controller/wewillgo.php
@@ -4,7 +4,7 @@ include_once 'model/wherewillwego.php';
 include_once 'model/content.php';
 include_once 'model/booktour.php';
 include_once 'model/login.php';
-// include_once 'model/xuly.php';
+include_once 'model/xuly.php';
 
 $action = 'index';
 if(isset($_GET['act'])){
@@ -19,14 +19,19 @@ switch($action){
     break;
 
     case 'content':
+
     $id=$_GET['idcontent'];
     $topFeautureById=getAllTopFeautureById($id);
-    /* $email_kh=$_SESSION['user']; *//* lấy email của khách hàng */
-    /* $khachhang=getkh_id($email_kh); */ /* từ email để truy cập vào database lấy thông ti của khách hàng */
-    /* $id_kh=$khachhang['id_kh']; *//* lấy id khách hàng */
-    /* $kh_dg=getKH_DG($id_kh,$id); */
+    $email_kh=$_SESSION['user'];/* lấy email của khách hàng */
+    $khachhang=getkh_id($email_kh); /* từ email để truy cập vào database lấy thông ti của khách hàng */
+    $id_kh=$khachhang['id_kh'];/* lấy id khách hàng */
+    $kh_dg=getKH_DG($id_kh,$id);
     include 'view/content/content.php';
+
+
     break;
+
+    
 
     case 'booktour':
         $id=$_GET['idcontent'];
