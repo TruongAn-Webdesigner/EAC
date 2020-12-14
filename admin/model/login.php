a1027837f174bc1e5ae18e17cb5311fa655cebe3
TruongAn-Webdesigner <71300924+TruongAn-Webdesigner@users.noreply.github.com>
2020-12-14 16:03:15 +0700

    Update login.php

--- admin/model/login.php
+++ admin/model/login.php
@@ -5,5 +5,10 @@
                 $sql="SELECT * FROM admin WHERE email = '$mail'";
                 return query($sql);
             }
+
+            function getadmin_dangky($name,$email,$sdt,$pass){
+                $sql="INSERT INTO `admin` (`name_ad`, `email`, `sdt`, `pass`) VALUES ('$name', '$email', '$sdt', '$pass')";
+                return execute($sql);
+            }
     
 ?>
