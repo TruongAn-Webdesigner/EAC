cfb7a1e2f377d8424c72befa5dfa8a27caf3defc
TruongAn-Webdesigner <71300924+TruongAn-Webdesigner@users.noreply.github.com>
2020-12-14 13:31:52 +0700

    Update xuly.php

--- site/model/xuly.php
+++ site/model/xuly.php
@@ -10,4 +10,18 @@
         $sql="SELECT count(*) as 'tong' FROM `rating` WHERE id_kh_dg = $id_kh and id_dd = $id";
         return query($sql);
     }
+
+    function getCM_By_ID_DD($id){
+      $sql="SELECT * FROM comment inner JOIN khachhang on comment.id_kh = khachhang.id_kh WHERE comment.id_dd=$id ORDER BY id_cm DESC";
+      return query($sql);
+  }
+
+  function addCM($id_kh,$id,$ngay_cm,$noidung_cm){
+    $sql="INSERT INTO comment(id_kh, id_dd, ngay_cm, noidung_cm) VALUES('$id_kh','$id','$ngay_cm','$noidung_cm')";
+    return execute($sql);
+  }
+
+  
+
+
 ?>
