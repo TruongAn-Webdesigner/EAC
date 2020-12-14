be95b6df020fd67e281939b16720e99d99bdc54b
TruongAn-Webdesigner <71300924+TruongAn-Webdesigner@users.noreply.github.com>
2020-12-14 16:03:31 +0700

    Update dangky.php

--- site/view/login/dangky.php
+++ site/view/login/dangky.php
@@ -3,32 +3,52 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LOGIN ADMIN</title>
-    <link rel="stylesheet" href="../../../admin/css/login.css">
+    <title>dangky</title>
+    <link rel="stylesheet" href="../../../site/css/dangky.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Signika&display=swap" rel="stylesheet">
 </head>
 <body>
 
-<form>
+<div class="rowww">
+<div class="box_bg">
+
+<div class="boxcenter_dangky">
+<div class="rowww">
+<div class="form">
+  <div class="text_dangky">Đăng ký</div>
+<form action="../../controller/login.php?act=dangky" method='post' enctype="multipart/form-data" onsubmit="return validate()">
+<div class="form-group">
+    <label for="exampleInputEmail1">Họ tên</label>
+    <input type="name" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="nhập họ tên">
+  </div>
   <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <input type="email" name="email"class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="nhập email">
     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
+
+  <div class="form-group">
+    <label for="exampleInputEmail1">Số điện thoại</label>
+    <input type="tel" name="sdt" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="nhập số điện thoại">
+  </div>
+
   <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <input type="password" name="pass" class="form-control" id="exampleInputPassword1" placeholder="Password">
   </div>
-  <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+
+
+  
+  <button type="submit" class="btn btn-primary">Gửi</button>
 </form>
 
-	<div id="dropDownSelect1"></div>
-
-
-
+<a href="">fsefse</a>
+  <div id="dropDownSelect1"></div>
+  </div>
+  </div>
+  </div>
+  </div>
+  </div>
 </body>
 </html>
