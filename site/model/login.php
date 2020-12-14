cb777ceffb3b92a6e86e6e3b00d3fbbe258828f4
TruongAn-Webdesigner <71300924+TruongAn-Webdesigner@users.noreply.github.com>
2020-12-14 16:03:29 +0700

    Update login.php

--- site/model/login.php
+++ site/model/login.php
@@ -10,3 +10,9 @@
                 $sql="SELECT * FROM khachhang WHERE email_kh = '$kh_login'";
                 return queryOne($sql);
             }
+
+            function getkh_dangky($name,$email,$sdt,$pass){
+                $sql="INSERT INTO `khachhang` (`name_kh`, `email_kh`, `sdt_kh`, `pass`) VALUES ('$name', '$email', '$sdt', '$pass')";
+                return execute($sql);
+            }
+?>
