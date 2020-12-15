1c3dc49455bf780731a92eca42fe8613e7b3103f
nguyenduccong <71300677+nguyenduccong@users.noreply.github.com>
2020-12-15 10:18:33 +0700

    Update booktour.php

--- site/model/booktour.php
+++ site/model/booktour.php
@@ -1,8 +1,16 @@
 <?php
-    function addToOrder($fname,$address,$email,$phone,$diadiem,$ngaydat)
+    function addToOrder($fname,$email,$phone,$diadiem,$ngaydat)
     {
-    $query="insert into booktour(hoten,email,sdt,diachi,id_dd,ngaydat)
-    values('$fname','$email','$phone','$address','$diadiem','$ngaydat')";
+    $query="insert into booktour(hoten,email,sdt,id_dd,ngaydat)
+    values('$fname','$email','$phone','$diadiem','$ngaydat')";
+    $lastId = executeReturnID($query);
+    return $lastId;//tra ve ma don hang moi vua them
+    }
+
+    function addToOrderlogin($fname,$email,$phone,$diadiem,$ngaydat,$id_kh)
+    {
+    $query="insert into booktour(hoten,email,sdt,id_dd,ngaydat,id_kh_login)
+    values('$fname','$email','$phone','$diadiem','$ngaydat',$id_kh)";
     $lastId = executeReturnID($query);
     return $lastId;//tra ve ma don hang moi vua them
     }
