dc064275cf0fbf53ecbe3d464dc3b4513aa05652
nguyenduccong <71300677+nguyenduccong@users.noreply.github.com>
2020-12-17 11:49:47 +0700

    Update index.php

--- admin/view/review/index.php
+++ admin/view/review/index.php
@@ -1,12 +1,44 @@
-<div class="place ml20">
-                <h1>PLACE</h1>
-                
+<div class="roww">
+    <div class="titleLT">
+            <span> Danh Sách các Địa Điểm</span>
+    </div>
 
-                <div class="box_count_review"></div>
-                <div class="box_count_view_review"></div>
-                <div class="rank_review"></div>
-                <a href="#" class="box_review"></a>
-                <a href="#" class="box_review"></a>
-                <a href="#" class="box_review"></a>
-                <a href="#" class="box_review"></a>
+</div>
+
+    <article>
+        <div class="roww">
+            <div class="full_bg">
+                <div class="boxcenter_content_tip">
+                            <div class="card-body">
+                                <table class="table table-striped table">
+                                
+                                    <tr>
+                                        <th>Mã tour</th>
+                                        <th>Địa điểm</th>
+                                        <th>Ngày đi</th>
+                                        <th>Số khách đặt</th>
+                                    </tr>
+                            
+                                 <?php
+
+                                    foreach($dd as $gt){
+                                        echo'
+                                            <tr>
+                                                <td>'.$gt['id_dd'].'</td>
+                                                <td>'.$gt['hoten'].'</td>
+                                                <td><a href="index.php?ctrl=usertourlist&act=chitiettour&id_dd='.$gt['id_dd'].'&name_dd='.$gt['name_dd'].'">'.$gt['name_dd'].'</a></td>
+                                                <td>'.$gt['ngaydat'].'</td>
+                                                <td><a href="index.php?ctrl=usertourlist&act=del&id_bt='.$gt['id_kh'].'&id_kh='.$gt['id_kh_login'].'"><button type="button" class="btn btn-danger">Del</button></a></td>
+                                            </tr>       
+                                        ';
+                                    }
+
+                                 ?>
+                                </table>
+                            </div>
+
+                </div>
+            </div>
         </div>
+    </article>
+</body>
