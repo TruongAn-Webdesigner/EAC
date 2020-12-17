a4ae2f575f00476f070d62c85ae7125214c455b5
nguyenduccong <71300677+nguyenduccong@users.noreply.github.com>
2020-12-17 11:49:22 +0700

    Update home.php

--- admin/model/home.php
+++ admin/model/home.php
@@ -8,28 +8,38 @@
         }
 
         function getCountKH(){
-            $sql="SELECT count(*) as tongKH from khachhang ";
+            $sql="SELECT count(*) as tongKH from booktour ";
             return query($sql);
         }
 
         function getTongTien(){
-          $sql="SELECT id_tour, chitiettour.price_ct*COUNT(*) AS tong FROM `danhsachkh` INNER JOIN chitiettour ON danhsachkh.id_ct = chitiettour.id_chitiet GROUP BY id_ct ";
+          $sql="SELECT diadiem.id_dd,diadiem.gia*COUNT(*) AS tong FROM `booktour` JOIN diadiem ON booktour.id_dd = diadiem.id_dd GROUP BY diadiem.id_dd ";
           return query($sql);
         }
 
         function getRankTour(){
-          $sql="SELECT * ,COUNT(*) AS 'tongKH' FROM (( diadiem JOIN chitiettour ON diadiem.id_dd =chitiettour.id_dd ) JOIN danhsachkh ON chitiettour.id_chitiet = danhsachkh.id_ct) GROUP BY id_ct ORDER BY `tongKH` desc";
+          $sql="SELECT *,COUNT(*) AS tongkh FROM `booktour` JOIN diadiem ON booktour.id_dd = diadiem.id_dd GROUP BY diadiem.id_dd ORDER BY `tongkh` desc";
           return query($sql);
         }
 
         function getRankKH(){
-          $sql="SELECT *,COUNT(*) AS tongCD FROM ((khachhang JOIN danhsachkh ON khachhang.id_kh = danhsachkh.id_kh )INNER JOIN chitiettour ON danhsachkh.id_ct = chitiettour.id_chitiet) GROUP BY danhsachkh.id_kh ORDER BY tongCD DESC";
+          $sql="SELECT *,COUNT(*) AS tongCD FROM khachhang JOIN booktour ON khachhang.id_kh = booktour.id_kh_login GROUP BY khachhang.id_kh";
           return query($sql);
         }
 
         function getAllTopFeautureById($id){
           $sql="SELECT * FROM noidungbocuc INNER JOIN diadiem on diadiem.id_dd=noidungbocuc.id_dd WHERE diadiem.id_dd='$id'";
           return query($sql);
+      } 
+
+      function getluotxem(){
+        $sql="SELECT * FROM `luotxem`";
+        return queryOne($sql);
+    }
+
+      function  updateluotxem($view){
+        $sql = "UPDATE luotxem SET view ='$view'";
+          execute($sql);
       }
 
 
