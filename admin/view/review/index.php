f3c31824cddf46b5119b19df4b762e82cf7a465b
nguyenduccong <71300677+nguyenduccong@users.noreply.github.com>
2020-12-17 22:28:19 +0700

    Update index.php

--- admin/view/review/index.php
+++ admin/view/review/index.php
@@ -1,6 +1,6 @@
 <div class="roww">
     <div class="titleLT">
-            <span> Danh Sách các Địa Điểm</span>
+            <span> Danh Sách các Địa Điểm Khách Đặt</span>
     </div>
 
 </div>
@@ -15,8 +15,9 @@
                                     <tr>
                                         <th>Mã tour</th>
                                         <th>Địa điểm</th>
-                                        <th>Ngày đi</th>
-                                        <th>Số khách đặt</th>
+                                        <th>Rating_dd</th>
+                                        <th>View</th>
+                                        <th>Số người đặt</th>
                                     </tr>
                             
                                  <?php
@@ -25,10 +26,10 @@
                                         echo'
                                             <tr>
                                                 <td>'.$gt['id_dd'].'</td>
-                                                <td>'.$gt['hoten'].'</td>
-                                                <td><a href="index.php?ctrl=usertourlist&act=chitiettour&id_dd='.$gt['id_dd'].'&name_dd='.$gt['name_dd'].'">'.$gt['name_dd'].'</a></td>
-                                                <td>'.$gt['ngaydat'].'</td>
-                                                <td><a href="index.php?ctrl=usertourlist&act=del&id_bt='.$gt['id_kh'].'&id_kh='.$gt['id_kh_login'].'"><button type="button" class="btn btn-danger">Del</button></a></td>
+                                                <td><a href="index.php?ctrl=review&act=chitiettour&id_dd='.$gt['id_dd'].'&name_dd='.$gt['name_dd'].'">'.$gt['name_dd'].'</a></td>
+                                                <td>'.$gt['rating_dd'].'</td>
+                                                <td>'.$gt['view'].'</td>
+                                                <td>'.$gt['tongkh'].'</td>
                                             </tr>       
                                         ';
                                     }
